bdc57e02dbb64e604873b206faebea1029fdd0dc
Caroline Thostrup <caro342h@stud.kea.dk>
2022-09-01 15:06:22 +0200

    basic set up of files

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.addEventListener("load", init());
+
+// model data
+const model = [17, 16, 19, 18, 8, 32, 25, 26, 22, 29, 16, 25, 10, 32, 4, 31, 1, 21, 23, 8, 22, 27, 3, 32, 1, 6, 8, 27, 1, 19, 1, 17, 25, 4, 0, 22, 28, 19, 32, 1];
+
+function init() {}
+function loop() {}
+function getNumberOfCustomers() {}
+function displayData() {}
+function modifyModel() {}
